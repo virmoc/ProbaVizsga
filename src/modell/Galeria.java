694e0f3edc815b0ba8306ef1c6d7fc7f0fa36657
virmoc <virmoc411@gmail.com>
2022-04-22 15:19:32 +0200

    objektum rendezése javítás

--- src/modell/Galeria.java
+++ src/modell/Galeria.java
@@ -5,20 +5,25 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Galeria {
     public static void main(String[] args) {
-//        List<String> sorok = Files.readAllLines(Paths.get(".txt"));
-//        List<KiallitasiTargy> targyak = new ArrayList();
-//        for(String sor : sorok){
-//                targyak.add(new KiallitasiTargy(sor));
-//            }
+        List<String> sorok = Files.readAllLines(Paths.get(".txt"));
+        List<KiallitasiTargy> targyak = new ArrayList();
+        for(String sor : sorok){
+                targyak.add(new KiallitasiTargy(sor));
+            }
         if(new File("kep.jpg").exists()){
             System.out.println("megjelenités folyamatban...");
         }else{
             System.out.println("nem lehet megjeleniteni");
         }
             
+        Collections.sort(targyak);
+           for (KiallitasiTargy targy : targyak) {
+                System.out.println(targy);
+            }
     }
 }
