7b158ba76a65b1640f9775643a60b1550ed85bc6
virmoc <virmoc411@gmail.com>
2022-04-22 15:15:25 +0200

    objektum rendezése

--- src/modell/Galeria.java
+++ src/modell/Galeria.java
@@ -1,14 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package modell;
 
-/**
- *
- * @author Toth.Virag
- */
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Galeria {
-    
+    public static void main(String[] args) {
+//        List<String> sorok = Files.readAllLines(Paths.get(".txt"));
+//        List<KiallitasiTargy> targyak = new ArrayList();
+//        for(String sor : sorok){
+//                targyak.add(new KiallitasiTargy(sor));
+//            }
+        if(new File("kep.jpg").exists()){
+            System.out.println("megjelenités folyamatban...");
+        }else{
+            System.out.println("nem lehet megjeleniteni");
+        }
+            
+    }
 }
--- src/modell/KiallitasiTargy.java
+++ src/modell/KiallitasiTargy.java
@@ -5,10 +5,49 @@
  */
 package modell;
 
+import java.util.Date;
+
+
+
 /**
  *
  * @author Toth.Virag
  */
 public abstract class KiallitasiTargy {
+
+//    public String format(Date date);
+    private String datum, keszito, cim;
+
+//    public KiallitasiTargy(String datum, String keszito, String cim) {
+//        this.datum = datum;
+//        this.keszito = keszito;
+//        this.cim = cim;
+//    }
+
+    public String getKeszito() {
+        return keszito;
+    }
+
+    public String getCim() {
+        return cim;
+    }
+
+    @Override
+    public String toString() {
+        return "KiallitasiTargy{" + "datum=" + datum + ", keszito=" + keszito + ", cim=" + cim + '}';
+    }
+    
+    @Override
+    public int compareTo(KiallitasiTargy o, KiallitasiTargy p) {
+        return this.keszito.compareTo(o.getKeszito());
+//        return this.cim.compareTo(p.getCim());
+    }
+    
+//    @Override
+//    public int compareTo(KiallitasiTargy p) {
+//        return this.cim.compareTo(p.getCim());
+//    }
+//    
+    
     
 }
